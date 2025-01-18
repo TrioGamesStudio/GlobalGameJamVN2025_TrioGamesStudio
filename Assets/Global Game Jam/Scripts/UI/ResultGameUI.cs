019d714a5e36088a5d47de0ba0b877e089f58081
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-01-18 16:20:59 +0700

    WIP

---
+++ Assets/Global Game Jam/Scripts/UI/ResultGameUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultGameUI : MonoBehaviour
+{
+    public TextMeshProUGUI resultText;
+    public Image shineGlowImg;
+    public float rotateZValue;
+    public float rotateSpeed = 5;
+    public float lerpingValue = 5;
+    private void Update()
+    {
+        rotateZValue += Time.deltaTime * rotateSpeed;
+        if (rotateZValue > 360 )
+            rotateZValue = 0;
+        Quaternion nextQuater = 
+            Quaternion.Euler(
+                shineGlowImg.transform.localRotation.x, 
+                shineGlowImg.transform.localRotation.y, 
+                rotateZValue);
+
+        Quaternion lerping =
+            Quaternion.Lerp(shineGlowImg.transform.localRotation, nextQuater, Time.deltaTime * lerpingValue);
+        shineGlowImg.transform.localRotation = lerping;
+    }
+
+    public void Complete()
+    {
+        resultText.text = "Complete";
+    }
+
+    public void Loose()
+    {
+        resultText.text = "Failed";
+    }
+}
